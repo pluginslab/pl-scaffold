a25d97f67405831dad63aa04434cfde2fcc638ce
Marcel Schmitz <schmitzoide@gmail.com>
2023-04-29 15:51:27 +0100

    phpcs and wpcs working properly now

--- plugins/pl-scaffold-plugin/pl-scaffold-plugin.php
+++ plugins/pl-scaffold-plugin/pl-scaffold-plugin.php
@@ -1,22 +1,22 @@
 <?php
-/*
-Plugin Name: Pluginslab Scaffold Plugin
-Plugin URI: http://pluginslab.com/
-Description: This is a scaffold plugin for WordPress that goes together with the scaffold theme.
-Version: 1.0
-Author: Marcel Schmitz
-Author URI: http://marcelschmitz.com/
-License: GPL-2.0+
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: pl-scaffold-plugin
-Domain Path: /languages
-*/
+/**
+ * Plugin Name: Pluginslab Scaffold Plugin
+ * Plugin URI: http://pluginslab.com/
+ * Description: This is a scaffold plugin that goes together with the scaffold theme.
+ * Version: 1.0
+ * Author: Marcel Schmitz
+ * Author URI: http://marcelschmitz.com/
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: pl-scaffold-plugin
+ * Domain Path: /languages
+ */
 
-// Ensure the plugin is being run from the correct folder
+// Ensure the plugin is being run from the correct folder.
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
-// Define constants
+// Define constants.
 define( 'PL_SCAFFOLD_PLUGIN_VERSION', '1.0' );
 define( 'PL_SCAFFOLD_PLUGIN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
