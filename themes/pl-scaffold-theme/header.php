0c93d1e34b756419baca43286de90a9d177bb0ba
pluginslab <hi@pluginslab.com>
2023-04-30 22:15:55 +0100

    added icons and favicon

--- themes/pl-scaffold-theme/header.php
+++ themes/pl-scaffold-theme/header.php
@@ -11,6 +11,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dist/images/icon-16.png" sizes="16x16">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
