52600467333323da171a02d2a50ebdeec0f7fdde
pluginslab <hi@pluginslab.com>
2023-05-06 11:12:55 +0100

    linting and cleaning up

--- plugins/pl-scaffold-plugin/src/blocks/example/block.php
+++ plugins/pl-scaffold-plugin/src/blocks/example/block.php
@@ -11,18 +11,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( !class_exists( 'pl_block_1' ) ) {
+if ( ! class_exists( 'PL_Block_1' ) ) {
 
-	class pl_block_1 {
-		
+	/**
+	 * Class pl_block_1
+	 */
+	class PL_Block_1 {
+
+		/**
+		 * Constructor.
+		 */
 		public function __construct() {
 			add_action( 'init', array( $this, 'register_block' ) );
 		}
-		
+
+		/**
+		 * Registers the block so that they can be enqueued through Gutenberg in
+		 * the corresponding context.
+		 *
+		 * @return void
+		 */
 		public function register_block() {
 			register_block_type( PL_SCAFFOLD_PLUGIN_PLUGIN_DIR . 'build/blocks/example' );
 		}
 	}
 }
 
-new pl_block_1();
+new PL_Block_1();
--- plugins/pl-scaffold-plugin/src/blocks/example/index.js
+++ plugins/pl-scaffold-plugin/src/blocks/example/index.js
@@ -6,15 +6,14 @@ import { registerBlockType } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
-import './editor.scss';
 import metadata from './block.json';
 
 // Register the block
-registerBlockType( metadata, {
-    edit() {
-        return <p> Hello world (from the editor)</p>;
-    },
-    save() {
-        return <p> Hola mundo (from the frontend) </p>;
-    },
+registerBlockType(metadata, {
+	edit() {
+		return <p> Hello world (from the editor)</p>;
+	},
+	save() {
+		return <p> Hola mundo (from the frontend) </p>;
+	},
 });
